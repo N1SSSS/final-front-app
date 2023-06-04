2989f7483048deb2b33af52e4652d784ac58e1ad
N1SSSS <st31sn1@gmail.com>
2023-06-04 16:36:02 +0300

    Final app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import './App.css';
+import { Routes, Route } from "react-router-dom";
 
-function App() {
+import SmoothScroll from "./components/SmoothScroll";
+import Layout from "./components/layout/Layout";
+import HomePage from "./components/home-page/HomePage";
+import BookingPage from "./components/booking-page/BookingPage";
+import NotFoundPage from "./components/404-page/NotFoundPage";
+
+export default function App() {
   return (
-    <div>
-      "Hello"
-    </div>
+    <SmoothScroll>
+      <Layout>
+        <Routes>
+          <Route
+            index
+            element={<HomePage />}
+          />
+          <Route
+            path="/"
+            element={<HomePage />}
+          />
+          <Route
+            path="/booking-page"
+            element={<BookingPage />}
+          />
+          <Route
+            path="*"
+            element={<NotFoundPage />}
+          />
+        </Routes>
+      </Layout>
+    </SmoothScroll>
   );
 }
-
-export default App;
